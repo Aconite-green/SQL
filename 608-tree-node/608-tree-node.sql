549612cb9b1e791187267561a0ad325593a3042e
SeongYong <pji50260@naver.com>
2022-08-08 15:31:24 +0900

    Time: 402 ms (81.04%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,20 +1,18 @@
-SELECT id, 'Root' AS 'type'
-FROM Tree
+SELECT id, 'Root' AS type FROM Tree
 WHERE p_id IS NULL
 
 UNION
 
-SELECT id, 'Inner' AS 'type'
-FROM Tree
+SELECT id, 'Leaf' AS type FROM Tree
 WHERE 
     p_id IS NOT NULL AND
-    id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
+    id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
 
 UNION
 
-SELECT id, 'Leaf' AS 'type'
-FROM Tree
+SELECT id, 'Inner' AS type FROM Tree
 WHERE
     p_id IS NOT NULL AND
-    id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
-ORDER BY id ASC;
+    id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL)
+
+ORDER BY id ASC
