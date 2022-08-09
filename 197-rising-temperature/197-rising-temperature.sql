8f7a588e2690ae570febb1608d7775174a08a6a6
SeongYong <pji50260@naver.com>
2022-08-09 21:34:04 +0900

    Time: 659 ms (37.86%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT wt1.id 
+FROM Weather wt1, Weather wt2
+WHERE 
+    wt1.temperature > wt2.temperature AND
+    TO_DAYS(wt1.recordDate) - TO_DAYS(wt2.recordDate) = 1;
+    
+  
